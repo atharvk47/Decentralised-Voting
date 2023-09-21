12b3964a35756c0b5af4e18b7506535a1ffa4f57
ThisisAtharv <atharv.kurlapkar47@gmail.com>
2023-09-21 23:36:46 +0530

    Changes modified.

---
+++ store/index.ts
@@ -0,0 +1,9 @@
+import { configureStore } from '@reduxjs/toolkit'; 
+import globalSlices from './globalSlices';
+
+
+export const store = configureStore({
+     reducer: {
+        globalStates: globalSlices,
+     }
+})
